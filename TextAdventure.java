ebf374426106e824b33be0e4c6fc5d61fbef0732
mcquest <80595817+mcquest@users.noreply.github.com>
2022-04-26 10:31:08 -0700

    Update TextAdventure.java

--- TextAdventure.java
+++ TextAdventure.java
@@ -14,8 +14,18 @@ public class TextAdventure
 {
 	public static void main(String[] args) throws InterruptedException
 	{
-			    // Function call
-				StreetChoice1();    
+		String name = user_input("name"); // Inputs "name" to user_input
+		String time = user_input("certain time"); // Inputs "certain time" to user_input
+		String minutes = user_input("number"); // Inputs "number" to user_input
+		String food = user_input("type of food"); // Inputs "type of food" to user_input
+		
+		// Prints the first phrase with name and time in it
+		System.out.println("Hello, my name is " + name + ". I wake up at " + time + " am and brush my teeth");
+		// Prints the second phrase with minutes and food in it
+		System.out.println("for " + minutes + " minutes. I then eat " + food + " and leave for school.");
+		
+		// Function call
+		StreetChoice1();    
 	}
 	
 	// Ask for user input from text and return the answer
