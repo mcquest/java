6572788c9191f9773c359a643b99aaaf3420a2fd
mcquest <80595817+mcquest@users.noreply.github.com>
2022-04-27 11:08:02 -0700

    Update Ref.java

--- Ref.java
+++ Ref.java
@@ -8,27 +8,44 @@ public class Ref
         	//Scanner scanObj = new Scanner(System.in);
         	//String name = scanObj.next();
 
-        	Manager mVariabkesCanBEANyName = new Manager();
+        	//Manager mVariabkesCanBEANyName = new Manager();
 		// Accessing a function with Dot notation 
-		mVariabkesCanBEANyName.HelloWorld();
+		//mVariabkesCanBEANyName.HelloWorld();
 		
 		java.util.concurrent.TimeUnit.SECONDS.sleep(1);
 		
 		int[] ia = {5,2,6,7,4,9,0};
 		//System.out.println(arrayMax(ia));
+
+		// Boolean Variable
 		boolean b1 = false;
-		System.out.println(SecondLargest(ia));
-		mergeSort(testA, testA.length);
+
+		// Send to Console
+		System.out.println(Algorithms.SecondLargest(ia));
+
+
+		//Algorithms.insertionSortImperativeA(testA);
+		//Algorithms.insertionSortRecursiveA(testA, testA.length);
+		//Algorithms.selectionSortAscending(testA);
+
+		//Merge Sort to sort an integer array in ascending order
+		//Algorithms.mergeSort(testA, testA.length);
+
+		//QuickSort Ascending Algorithm 
+		//Algorithms.quickPivotSort(testA, 0, testA.length-1);
+
 		for (int n: testA)
 		{
 			System.out.print(n);
 		}
 		
+
 		/*
 		 * How Scanners function in Loops
 		 */
 		Scanner scan = new Scanner(System.in);
 		scan.close();
+
 		
 	}
 	
@@ -37,121 +54,4 @@ public class Ref
 		      {'o','x','-'},
 		      {'o','x','-'}};
 	
-	// Maximum integer from an array 
-	// and change comparison for Min
-	static int arrayMax(int[] arrayI)
-	{
-		// Temp variable to store smallest value
-		int tempMax = arrayI[0];
-		// For loop 
-		for (int i =1; i < arrayI.length; i++)
-		{
-			if (arrayI[i] > tempMax)
-			{
-				// What is missing here????????????
-				tempMax = arrayI[i];
-			}
-		}
-		
-		// Return Max
-		return(tempMax);
-				
-	}
-	// second largest int in an array 
-	
-	static int SecondLargest(int[] a )
-	{
-		
-		if (a.length == 0 | a.length == 1)
-		{
-			return -0;
-		}
-		
-		
-		else
-		{
-			int l2; 
-			int l1;
-			
-			if (a[0] > a[1])
-			{
-				l2 = a[1];
-				l1 = a[0];
-				
-			}
-			else
-			{
-				l2 = a[0];
-				l1 = a[1];
-			}
-			
-			for (int i: a)
-			{
-				//System.out.println(i);
-				if (i > l1)
-				{
-					l2 = l1;
-					l1 = i;
-					
-				}
-				else if(i > l2)
-				{
-					l2 = a[i];
-				}
-			}
-			
-			return l2;
-		}
-		
-	}
-	
-	
-	public static void mergeSort(int[] a, int n) 
-	{
-		if (n < 2)
-		{
-			return;
-		}
-		int mid = n / 2;
-		int[] l = new int[mid];
-		int[] r = new int[n - mid];
-	
-		for (int i = 0; i < mid; i++) 
-		{
-			l[i] = a[i];
-		}
-		for (int i = mid; i < n; i++) 
-		{
-			r[i - mid] = a[i];
-		}
-		mergeSort(l, mid);
-		mergeSort(r, n - mid);
-	
-		merge(a, l, r, mid, n - mid);
-	}
-
-	public static void merge(int[] a, int[] l, int[] r, int left, int right) 
-	{
- 
-    		int i = 0, j = 0, k = 0;
-    		while (i < left && j < right) 
-		{
-        		if (l[i] <= r[j]) 
-			{
-            			a[k++] = l[i++];
-        		}
-        		else 
-			{
-            			a[k++] = r[j++];
-        		}
-    		}
-    		while (i < left) 
-		{
-        		a[k++] = l[i++];
-    		}
-    		while (j < right) 
-		{
-        		a[k++] = r[j++];
-    		}
-	}
 }
